252fce0f52fe845dd0633e2b3c57dd1319f4d9d3
Isai <castanedaisai303@gmail.com>
2022-08-30 11:48:41 -0700

    commit tree 01cbbe46dda7e993708d1293fda9f180a7ad36ab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(start) {
+  if(start > 42) {
+return start - 42;
+  } else if(start < 42){
+    return 42 - start;
+  }
+  console.log(`${start}`);
+}
+
+function distanceFromHqInFeet(start){
+  if(start > 42){
+    return (start - 42)*264;
+  } else if(start < 42){
+    return (42 - start)*264;
+  }
+}
+
+function distanceTravelledInFeet(start, destination){
+if( start < destination){
+  return (destination - start)*264;
+} else if(start > destination){
+  return (start - destination)*264;
+}
+}
+
+function calculatesFarePrice(start, destination){
+  if (start < destination){
+    return (destination - start)-1;
+  } else if (start > destination){
+    return(start - destination)+.56;
+  }
+  return 25;
+}
